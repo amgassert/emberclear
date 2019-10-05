99b8ab26a4c6462cd047433d70cece09c0587243
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-10-05 00:19:51 -0400

    enable and upgrade embroider

--- packages/frontend/ember-cli-build.js
+++ packages/frontend/ember-cli-build.js
@@ -169,15 +169,15 @@ module.exports = function(defaults) {
   });
 
   // Embroider is too buggy atm
-  // return require('@embroider/compat').compatBuild(app, require('@embroider/webpack').Webpack, {
-  //   extraPublicTrees: [qrScannerWorker],
-  //   // staticAddonTestSupportTrees: true,
-  //   // staticAddonTrees: true,
-  //   // staticHelpers: true,
-  //   // staticComponents: true,
-  //   // splitAtRoutes: true,
-  //   // skipBabel: [],
-  // });
+  return require('@embroider/compat').compatBuild(app, require('@embroider/webpack').Webpack, {
+    extraPublicTrees: [qrScannerWorker],
+    // staticAddonTestSupportTrees: true,
+    // staticAddonTrees: true,
+    // staticHelpers: true,
+    // staticComponents: true,
+    // splitAtRoutes: true,
+    // skipBabel: [],
+  });
 
-  return mergeTrees([app.toTree(), qrScannerWorker]);
+  // return mergeTrees([app.toTree(), qrScannerWorker]);
 };
