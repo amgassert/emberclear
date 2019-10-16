6f566ce7494400506585f61e6e3b3f31fdfff7c6
Hussein Alawami <alawamhm@rose-hulman.edu>
2019-10-16 00:44:50 -0400

    fix code

--- packages/frontend/app/pods/components/pod/application/off-canvas-container/sidebar/contacts/contact/component.ts
+++ packages/frontend/app/pods/components/pod/application/off-canvas-container/sidebar/contacts/contact/component.ts
@@ -32,7 +32,7 @@ export default class SidebarContact extends Component<IArgs> {
 
   @reads('settings.hideOfflineContacts') hideOfflineContacts!: boolean;
 
-  get getIsPinned(){
+  get isPinned() {
     const { contact } = this.args;
 
     return contact.isPinned;
@@ -62,7 +62,7 @@ export default class SidebarContact extends Component<IArgs> {
     }
 
     // always show if contact is pinned
-    if(contact.isPinned){
+    if (contact.isPinned) {
       return true;
     }
 
@@ -80,7 +80,7 @@ export default class SidebarContact extends Component<IArgs> {
     return this.unreadMessages.length;
   }
 
-  @task(function*(this: SidebarContact) {
+  @task(function* (this: SidebarContact) {
     let { contact } = this.args;
     let messages = yield this.store.findAll('message');
     let unread = selectUnreadDirectMessages(messages, contact.id);
@@ -97,14 +97,15 @@ export default class SidebarContact extends Component<IArgs> {
     this.router.transitionTo('chat.privately-with', this.args.contact.id);
   }
 
-  @action onPinClick() {
+  @action onPin() {
     const { contact } = this.args;
     contact.set('isPinned', !contact.isPinned);
     contact.save();
   }
 
-  get isOtherContacts() {
+  get canBePinned() {
     const { contact } = this.args;
+    // can't pin your own chat
     return contact.id !== currentUserId;
   }
 }
